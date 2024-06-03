760d43b8fd387a7a01a9fe16a48d3c1ee376ed7b
Teethew <thiagjack@gmail.com>
2024-06-03 08:47:52 -0300

    zerolog

--- go.mod
+++ go.mod
@@ -2,18 +2,9 @@ module github.com/zero-identity/0idc
 
 go 1.22.3
 
-require github.com/go-oauth2/oauth2/v4 v4.5.2
-
 require (
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/tidwall/btree v1.7.0 // indirect
-	github.com/tidwall/buntdb v1.3.1 // indirect
-	github.com/tidwall/gjson v1.17.1 // indirect
-	github.com/tidwall/grect v0.1.4 // indirect
-	github.com/tidwall/match v1.1.1 // indirect
-	github.com/tidwall/pretty v1.2.1 // indirect
-	github.com/tidwall/rtred v0.1.2 // indirect
-	github.com/tidwall/tinyqueue v0.1.1 // indirect
-	golang.org/x/net v0.25.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,57 +1,72 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"os"
 
-	"github.com/go-oauth2/oauth2/v4/errors"
-	"github.com/go-oauth2/oauth2/v4/manage"
-	"github.com/go-oauth2/oauth2/v4/models"
-	"github.com/go-oauth2/oauth2/v4/server"
-	"github.com/go-oauth2/oauth2/v4/store"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	manager := manage.NewDefaultManager()
-	// token memory store
-	manager.MustTokenStorage(store.NewMemoryTokenStore())
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	// client memory store
-	clientStore := store.NewClientStore()
-	clientStore.Set("000000", &models.Client{
-		ID:     "000000",
-		Secret: "999999",
-		Domain: "http://localhost",
-	})
-	manager.MapClientStorage(clientStore)
+	log.Info().Msg("hello world")
+	// manager := manage.NewDefaultManager()
+	// manager.SetClientTokenCfg(&manage.Config{
+	// 	AccessTokenExp:    10 * time.Minute,
+	// 	IsGenerateRefresh: false,
+	// })
 
-	srv := server.NewDefaultServer(manager)
-	srv.SetAllowGetAccessRequest(true)
-	srv.SetClientInfoHandler(server.ClientFormHandler)
+	// mongoConf := mongo.NewConfigNonReplicaSet(
+	// 	"mongodb://127.0.0.1:27017",
+	// 	"oauth2", // database name
+	// 	"root",
+	// 	"secret",
+	// 	"serviceName",
+	// )
 
-	srv.UserAuthorizationHandler = func(w http.ResponseWriter, r *http.Request) (userID string, err error) {
-		return "000000", nil
-	}
+	// // use mongodb token store
+	// manager.MapTokenStorage(mongo.NewTokenStore(mongoConf))
 
-	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
-		log.Println("Internal Error:", err.Error())
-		return
-	})
+	// // client memory store
+	// clientStore := mongo.NewClientStore(mongoConf)
+	// manager.MapClientStorage(clientStore)
 
-	srv.SetResponseErrorHandler(func(re *errors.Response) {
-		log.Println("Response Error:", re.Error.Error())
-	})
+	// clientStore.Create(&models.Client{
+	// 	ID:     "clientid",
+	// 	Secret: "secret",
+	// 	Domain: "somedomain",
+	// 	Public: false,
+	// 	UserID: "test",
+	// })
 
-	http.HandleFunc("/authorize", func(w http.ResponseWriter, r *http.Request) {
-		err := srv.HandleAuthorizeRequest(w, r)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-		}
-	})
+	// srv := server.NewDefaultServer(manager)
+	// srv.SetAllowGetAccessRequest(true)
+	// srv.SetClientInfoHandler(server.ClientFormHandler)
 
-	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
-		srv.HandleTokenRequest(w, r)
-	})
+	// srv.UserAuthorizationHandler = func(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+	// 	return "clientid", nil
+	// }
 
-	log.Fatal(http.ListenAndServe(":9096", nil))
+	// srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
+	// 	log.Println("Internal Error:", err.Error())
+	// 	return
+	// })
+
+	// srv.SetResponseErrorHandler(func(re *errors.Response) {
+	// 	log.Println("Response Error:", re.Error.Error())
+	// })
+
+	// http.HandleFunc("/authorize", func(w http.ResponseWriter, r *http.Request) {
+	// 	err := srv.HandleAuthorizeRequest(w, r)
+	// 	if err != nil {
+	// 		http.Error(w, err.Error(), http.StatusBadRequest)
+	// 	}
+	// })
+
+	// http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+	// 	srv.HandleTokenRequest(w, r)
+	// })
+
+	// log.Fatal(http.ListenAndServe(":9096", nil))
 }
